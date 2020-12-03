c12d0803853c6f568398f74da62a13f05793ea89
EwenQuim <ewen.quimerch@gmail.com>
2020-12-03 02:14:27 +0100

    docs: screenshots

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -8,7 +8,7 @@ export default function TabTwoScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>Realized by Ewen Quimerc'h </Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text style={styles.title}>for FreshPlanet</Text>
+      <Text style={styles.title}>in React Native</Text>
     </View>
   );
 }
