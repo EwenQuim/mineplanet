df7cbb70173975aed4abd4802b6f684c7ab4c65c
EwenQuim <ewen.quimerch@gmail.com>
2020-12-14 15:48:18 +0100

    feat: leaderboard display

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,14 +1,86 @@
+import axios from 'axios';
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Button, FlatList, StyleSheet } from 'react-native';
 
 import { Text, View } from '../components/Themed';
+import { ScoreLine } from '../types';
+
+interface ServerResponse {
+  data: ScoreLine[]
+}
 
 export default function TabTwoScreen() {
+
+  let [scores, setScores] = useState<ScoreLine[]>([])
+  let [canDisplay, setCanDisplay] = useState(false)
+
+  useEffect(() => {
+    getData()
+    console.log("get data");
+
+  }, [])
+  useEffect(() => {
+    setCanDisplay(true)
+    console.log("canDisplay");
+  }, [scores])
+
+
+
+  const getData = () => {
+    axios.get('https://minebackend.herokuapp.com/leaderboard', {
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+      .then((response) => {
+        setScores(response.data)
+      })
+      .catch((err) => console.error(err))
+
+  }
+
+  const postScore = (scoreLine: ScoreLine) => {
+    console.log("posting score");
+    axios.post('https://minebackend.herokuapp.com/leaderboard', scoreLine)
+      .then((response) => console.log(response.data))
+      .catch((err) => console.error(err))
+  }
+
+  // useEffect(() => {
+  //   getData();
+  // }, []);
+  const displayScores = () => {
+    if (canDisplay) {
+      return <FlatList
+        data={scores}
+        keyExtractor={item => item.date.toString()}
+        renderItem={({ item: score, index }: { item: ScoreLine, index: number }) => {
+
+          return <Text>{index + 1} - {score.name} : {score.score} points</Text>
+
+        }}
+      />
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Realized by Ewen Quimerc'h </Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <Text style={styles.title}>in React Native</Text>
+      <Button title={"Get Scores"} onPress={() => getData()} />
+      <Button title={"Post Score"} onPress={
+        () => postScore({
+          name: "Ewen",
+          score: 250,
+          time: 30,
+          date: new Date(), //placeholder: real date is generated in the back when received 
+        })
+      } />
+
+
+      {displayScores()}
     </View>
   );
 }
