89221b8edeb31c4bf3041fc0f577cfc4d28806b0
EwenQuim <ewen.quimerch@gmail.com>
2020-12-22 13:40:41 +0100

    feat: fancy separator

--- components/Separator.tsx
+++ components/Separator.tsx
@@ -1,21 +1,14 @@
-
-import React from "react";
+import React from 'react';
 
 import { View } from '../components/Themed';
-import { StyleSheet } from 'react-native';
 
-
-const Sep = () => {
-    return <View style={styles.separator} />
-}
-
-const styles = StyleSheet.create({
-    separator: {
-        marginVertical: 20,
-        height: 1,
-        width: '80%',
-        backgroundColor: "#8888",
-    },
-});
-
-export default Sep
+export default () => (
+  <View
+    style={{
+      marginVertical: 20,
+      height: 1,
+      width: '80%',
+      backgroundColor: '#8888'
+    }}
+  />
+);
