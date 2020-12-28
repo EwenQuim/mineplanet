cde3b87f20facc174b716b51913a4f8f0095d727
EwenQuim <ewen.quimerch@gmail.com>
2020-12-28 06:17:21 +0100

    feat: computing scores without errors

--- utils/computeScore.ts
+++ utils/computeScore.ts
@@ -1,10 +1,11 @@
-import Board from "../Board";
-import { GameState } from "../types";
+import Board from '../Board';
+import { GameState } from '../types';
 
 export const computeScore = (board: Board, seconds: number): number => {
   if (board.gameState === GameState.Won) {
     return Math.round(
-      (10000 * board.bombsTotal) / (board.width * board.height * seconds)
+      (1000 * board.bombsTotal ** 2) /
+        (board.width * board.height * (seconds + 1))
     );
   } else {
     return 0;
