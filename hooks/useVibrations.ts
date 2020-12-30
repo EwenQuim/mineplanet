df2102bd5fe430a329c4245f1f5381f59062436a
EwenQuim <ewen.quimerch@gmail.com>
2020-12-30 23:29:05 +0100

    fix: vibrations issue on end

--- hooks/useVibrations.ts
+++ hooks/useVibrations.ts
@@ -4,19 +4,19 @@ import { GameState } from '../types';
 export const vibrateOnTouch = (shortPress?: boolean, state?: GameState) => {
   if (shortPress) {
     Vibration.vibrate(50);
-    switch (state) {
-      case GameState.Lost:
-        Vibration.vibrate(2000);
-        break;
-
-      case GameState.Won:
-        Vibration.vibrate(Array(12).fill(100));
-        break;
-
-      default:
-        break;
-    }
   } else if (shortPress !== undefined) {
     Vibration.vibrate([0, 50, 50, 50]);
+  }
+  switch (state) {
+    case GameState.Lost:
+      Vibration.vibrate(2000);
+      break;
+
+    case GameState.Won:
+      Vibration.vibrate(Array(12).fill(100));
+      break;
+
+    default:
+      break;
   }
 };
